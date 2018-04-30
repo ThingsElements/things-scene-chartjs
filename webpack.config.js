86cfd8370dee4909a14bd09c8d5ab69caa8ca70c
Hearty Oh <heartyoh@hatiolab.com>
2018-05-01 00:13:25 +0800

    3.0.6
    things-shell-integration 적용.

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    "things-scene-chartjs": ['./src/index.js']
+  },
+  output: {
+    path: path.resolve('./dist'),
+    filename: '[name].js',
+  },
+  resolve: {
+    modules: ['./node_modules']
+  },
+  resolveLoader: {
+    modules: ['./node_modules']
+  },
+  externals: {
+    "@hatiolab/things-scene": "scene",
+    "chart.js": "Chart",
+    "tinycolor2": "tinycolor"
+  },
+  optimization: {
+    minimize: true,
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              'es2015', {
+                targets: {
+                  browsers: ['last 2 Chrome versions', 'Safari 10']
+                },
+                debug: true
+              }
+            ]
+          ],
+          plugins: []
+        }
+      }
+    }, {
+      test: /\.(gif|jpe?g|png)$/,
+      loader: 'url-loader?limit=25000',
+      query: {
+        limit: 10000,
+        name: '[path][name].[hash:8].[ext]'
+      }
+    }, {
+      test: /\.(obj|mtl|tga|3ds|max|dae)$/,
+      use: [{
+        loader: 'file-loader',
+        options: {}
+      }]
+    }]
+  },
+  plugins: [
+    new UglifyJsPlugin({
+      test: /\-min\.js$/
+    })
+  ],
+  devtool: 'cheap-module-source-map'
+}
